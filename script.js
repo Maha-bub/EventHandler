e9d912b88cd5292a2babd8139ca012e5095af191
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-03-06 23:47:35 +0600

    option-1 for event related problem

--- script.js
+++ script.js
@@ -8,23 +8,3 @@ const makeBlueButton = document.getElementById("makeBlue");
 makeBlueButton.onclick = function makeBlue() {
   document.body.style.backgroundColor = "blue";
 };
-
-// option-3 (version-2)
-const purpleButton = document.getElementById("MakePurple");
-purpleButton.onclick = function purpleButton() {
-  document.body.style.backgroundColor = "purple";
-};
-// option-4
-
-const pinkButton = document.getElementById("make-pink");
-pinkButton.addEventListener("click", makePink);
-function makePink() {
-  document.body.style.backgroundColor = "pink";
-}
-
-// option-4 another one(function declared inside the add even lisener )
-
-const makeGreen = document.getElementById("make-green");
-makeGreen.addEventListener("click", function makeGreen() {
-  document.body.style.backgroundColor = "green";
-});

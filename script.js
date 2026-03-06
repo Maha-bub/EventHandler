d58b9cb8281a75bd88908ce248058a4ebb7cb93b
Your Name <you@example.com>
2026-03-06 23:33:15 +0600

    get element by id by direct function write in onclick effect

--- script.js
+++ script.js
@@ -3,9 +3,28 @@
 function makeRed() {
   document.body.style.backgroundColor = "red";
 }
+// option-3 (version-1)
 const makeBlueButton = document.getElementById("makeBlue");
-
-
 makeBlueButton.onclick = function makeBlue() {
   document.body.style.backgroundColor = "blue";
 };
+
+// option-3 (version-2)
+const purpleButton = document.getElementById("MakePurple");
+purpleButton.onclick = function purpleButton() {
+  document.body.style.backgroundColor = "purple";
+};
+// option-4
+
+const pinkButton = document.getElementById("make-pink");
+pinkButton.addEventListener("click", makePink);
+function makePink() {
+  document.body.style.backgroundColor = "pink";
+}
+
+// option-4 another one(function declared inside the add even lisener)
+
+const makeGreen = document.getElementById("make-green");
+makeGreen.addEventListener("click", function makeGreen() {
+  document.body.style.backgroundColor = "green";
+});
